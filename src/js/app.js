fef16fd40554c3e86c378bfe37418719065e7535
jakobev <jakobev1@gmail.com>
2026-04-03 17:52:25 +0200

    added code and audio files with lfs

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+var farts_container = document.getElementsByClassName("farts-container")
+
+let currentAudio = null
+
+for(let i = 1; i<=100; i++){
+    var test_button = document.createElement('button')
+    var audio_fart = document.createElement("audio")
+    audio_fart.id = "fart" + i
+    audio_fart.src = "src/data/fart_dataset/" + i + ".wav"
+    console.log(audio_fart.src)
+    test_button.addEventListener("click", () => {
+        /*audio_fart.play()*/
+        var audio = document.getElementById("fart" + i)
+
+        if(currentAudio){
+            currentAudio.pause()
+            currentAudio.currentTime = 0
+        }
+        audio.play()
+        currentAudio = audio
+    });
+
+    test_button.textContent = "Fart" + i
+    farts_container[0].appendChild(test_button)
+    farts_container[0].appendChild(audio_fart)
+}
